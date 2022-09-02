0507d2028e596e6c46d7fc5c050d0b1977e51c00
David Anthony <david.anthony@swri.org>
2022-09-02 09:17:48 -0500

    More work

--- gpsd_client/src/gpsd_parser_v9.cpp
+++ gpsd_client/src/gpsd_parser_v9.cpp
@@ -34,7 +34,97 @@ bool GpsdParserV9::getData(
 
   fix_msg.header.stamp.sec = p->fix.time.tv_sec;
   fix_msg.header.stamp.nanosec = p->fix.time.tv_nsec;
+  fix_msg.err_time = p->fix.ept;
   fix_msg.altitude = p->fix.altitude;
+  fix_msg.err_vert = p->fix.epv;
+  fix_msg.latitude = p->fix.latitude;
+  fix_msg.err_latitude = p->fix.epy;
+  fix_msg.longitude = p->fix.longitude;
+  fix_msg.err_longitude = p->fix.epx;
+  fix_msg.err_position = p->fix.eph;
+  fix_msg.track = p->fix.track;
+  fix_msg.err_track = p->fix.epd;
+  fix_msg.track_magnetic = p->fix.magnetic_track;
+  fix_msg.magnetic_var = p->fix.magnetic_var;
+  fix_msg.speed = p->fix.speed;
+  fix_msg.err_speed = p->fix.eps;
+  fix_msg.climb = p->fix.climb;
+  fix_msg.err_climb = p->fix.epc;
+  fix_msg.gdop = p->dop.gdop;
+  fix_msg.hdop = p->dop.hdop;
+  fix_msg.vdop = p->dop.vdop;
+  fix_msg.tdop = p->dop.tdop;
+
+  switch (p->fix.mode)
+  {
+    case MODE_NOT_SEEN:
+      status_msg.mode = gps_msgs::msg::GNSSStatus::GNSS_MODE_NOT_SEEN;
+      break;
+
+    case MODE_NO_FIX:
+      status_msg.mode = gps_msgs::msg::GNSSStatus::GNSS_MODE_NO_FIX;
+      break;
+
+    case MODE_2D:
+      status_msg.mode = gps_msgs::msg::GNSSStatus::GNSS_MODE_2D;
+      break;
+
+    case MODE_3D:
+      status_msg.mode = gps_msgs::msg::GNSSStatus::GNSS_MODE_3D;
+      break;
+
+    default:
+      status_msg.mode = gps_msgs::msg::GNSSStatus::GNSS_MODE_UNKNOWN;
+      break;
+  }
+
+  switch (p->status)
+  {
+    case STATUS_NO_FIX:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_NONE;
+      break;
+
+    case STATUS_FIX:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_BASIC;
+      break;
+
+    case STATUS_DGPS_FIX:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_DGPS;
+      break;
+
+    case STATUS_RTK_FIX:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_RTK_FIXED;
+      break;
+
+    case STATUS_RTK_FLT:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_RTK_FLOATING;
+      break;
+
+    case STATUS_DR:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_DEAD_RECKONING;
+      break;
+
+    case STATUS_GNSSDR:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_GNSS_DEAD_RECKONING;
+      break;
+
+    case STATUS_TIME:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_TIME_SURVEYED;
+      break;
+
+    case STATUS_SIM:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_SIMULATED;
+      break;
+    
+    case STATUS_PPS_FIX:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_P_Y_CODE;
+      break;
+
+    default:
+      fix_msg.fix_type = gps_msgs::msg::GNSSFix::FIX_UNKNOWN;
+      break;
+  }
+
   status_msg.num_satellites_used = p->satellites_used;
   status_msg.num_satellites_unused = std::max(
     0,
@@ -73,13 +163,34 @@ bool GpsdParserV9::getData(
     {
       current_sat->gnss_id = p->skyview[visible_idx].gnssid;
       current_sat->sv_id = p->skyview[visible_idx].svid;
-      current_sat->freq_id = p->skyview[visible_idx].freqid;
+      current_sat->freq_id = map_freq(
+        current_sat->gnss_id,
+        p->skyview[visible_idx].freqid);
     }
     else
     {
       current_sat->sv_id = gps_msgs::msg::GNSSSatellite::GNSS_INVALID_SV_ID;
       current_sat->gnss_id = gps_msgs::msg::GNSSSatellite::GNSS_ID_INVALID;
       current_sat->freq_id = gps_msgs::msg::GNSSSatellite::SIG_ID_INVALID;
+    }
+
+    switch (p->skyview[visible_idx].health)
+    {
+      case SAT_HEALTH_UNK:
+        current_sat->health = gps_msgs::msg::GNSSSatellite::GNSS_SAT_HEALTH_UNKNOWN;
+        break;
+
+      case SAT_HEALTH_BAD:
+        current_sat->health = gps_msgs::msg::GNSSSatellite::GNSS_SAT_HEALTH_BAD;
+        break;
+
+      case SAT_HEALTH_OK:
+        current_sat->health = gps_msgs::msg::GNSSSatellite::GNSS_SAT_HEALTH_OK;
+        break;
+
+      default:
+        current_sat->health = gps_msgs::msg::GNSSSatellite::GNSS_SAT_HEALTH_UNKNOWN;
+        break;
     }
     
   }
@@ -88,6 +199,8 @@ bool GpsdParserV9::getData(
 
 int8_t GpsdParserV9::map_freq(const int8_t gnss_id, const int8_t freq_id)
 {
+  int8_t ret_value = gps_msgs::msg::GNSSSatellite::SIG_ID_UNKNOWN;
+
   const std::map<int8_t, std::map<int8_t, int8_t>> freq_map =
   {
     {gps_msgs::msg::GNSSSatellite::GNSS_ID_GPS,
@@ -127,12 +240,23 @@ int8_t GpsdParserV9::map_freq(const int8_t gnss_id, const int8_t freq_id)
     },
     {gps_msgs::msg::GNSSSatellite::GNSS_ID_GLONASS,
       {
-      {0, gps_msgs::msg::GNSSSatellite::SIG_ID_GLONASS_L1_OF},
-      {2, gps_msgs::msg::GNSSSatellite::SIG_ID_GLONASS_L2_OF},
+      {1, gps_msgs::msg::GNSSSatellite::SIG_ID_GLONASS_L1_OF},
+      {3, gps_msgs::msg::GNSSSatellite::SIG_ID_GLONASS_L2_OF},
       }
     }
   };
 
-  return 0;
+  auto gnss_iter = freq_map.find(gnss_id);
+
+  if (gnss_iter != freq_map.end())
+  {
+    auto sig_iter = gnss_iter->second.find(freq_id);
+    if (sig_iter != gnss_iter->second.end())
+    {
+      ret_value = sig_iter->second;
+    }
+  }
+
+  return ret_value;
 }
 }
